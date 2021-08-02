9052308264d39f84c244ac93c0aee3919fbaef27
waynebonc <24826299+waynebonc@users.noreply.github.com>
2021-08-02 11:40:58 +0200

    Add IMG3 image identification and decryption.

--- autodecrypt/decrypt_img.py
+++ autodecrypt/decrypt_img.py
@@ -1,4 +1,4 @@
-"""Module used for decrypting and extracting img4 file format"""
+"""Module used for decrypting and extracting img3 and img4 file formats"""
 import logging
 import os
 import subprocess
@@ -6,11 +6,18 @@ import sys
 
 
 def get_image_type(filename: str):
-    """Check if it is IMG4 format."""
+    """Check if it is an IMG3 or IMG4 format."""
     if not os.path.isfile(filename):
         print("[e] %s : file not found" % filename)
         sys.exit(-1)
     with open(filename, "rb") as file:
+        magic = file.read(4)
+        if magic == b"3gmI":
+            magic = "img3"
+            file.seek(12, os.SEEK_CUR)
+            img_type = file.read(4)[::-1]
+            return magic, img_type
+        
         file.seek(7, 0)
         magic = file.read(4)
         if magic == b"IM4P":
@@ -23,27 +30,40 @@ def get_image_type(filename: str):
 
 
 def decrypt_img(infile: str, magic: str, key: str, init_vector: str):
-    """Decrypt IMG4 image file."""
+    """Decrypt IMG3 or IMG4 image file."""
     image_type = get_image_type(infile)
     if image_type is None:
-        print("[e] %s is not an IMG4 file" % infile)
+        print("[e] %s is not an IMG3 or IMG4 file" % infile)
         sys.exit(1)
 
-    outfile = infile.replace("im4p", "bin")
-    if magic == "img4":
+    outfile = infile.replace("im4p", "bin").replace("dfu", "bin")
+
+    if magic == "img3" or magic == "img4":
         print("[i] decrypting %s to %s..." % (infile, outfile))
         fnull = open(os.devnull, "w")
 
         ivkey = init_vector + key
-        logging.info("img4 -i %s %s %s", infile, outfile, ivkey)
 
-        try:
-            subprocess.Popen(
-                ["img4", "-i", infile, outfile, ivkey], stdout=fnull
-            )
-        except FileNotFoundError:
-            print("[e] can't decrypt file, is img4 tool in $PATH ?")
-            sys.exit(1)
+        if magic == "img4":
+            logging.info("img4 -i %s %s %s", infile, outfile, ivkey)
+
+            try:
+                subprocess.Popen(
+                    ["img4", "-i", infile, outfile, ivkey], stdout=fnull
+                )
+            except FileNotFoundError:
+                print("[e] can't decrypt file, is img4 tool in $PATH ?")
+                sys.exit(1)
+        else:
+            logging.info("xpwntool %s %s -iv %s -k %s", infile, outfile, init_vector, key)
+
+            try:
+                subprocess.Popen(
+                    ["xpwntool", infile, outfile, "-iv", init_vector, "-k", key], stdout=fnull
+                )
+            except FileNotFoundError:
+                print("[e] can't decrypt file, is xpwntool in $PATH ?")
+                sys.exit(1)
     else:
         print("{} is not supported".format(magic))
         return
